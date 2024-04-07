080da02a23e30ce285f8d0a28f861c867b504014
Raz-Amouyal <158087547+Raz-Amouyal@users.noreply.github.com>
2024-04-07 22:51:03 +0300

    Update digit_freq.c

--- digit_freq.c
+++ digit_freq.c
@@ -11,12 +11,16 @@ int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int counter[NUM_OF_DIGITS]={0};
-    char temp;
-    while (scanf("%c", &temp) == 1){
-        if (temp >= '0' && temp <= '9'){
-            counter[temp - '0']++;
+    char str[MAX_STRING_SIZE];
+    fgets(str, MAX_STRING_SIZE, stdin);
+    int l1 = strlen(str);
+    str[l1 - 1] = '\0';
+    for (int i = 0; i < l1; ++i) {
+        if (str[i] >= '0' && str[i] <= '9'){
+            counter[str[i] - '0']++;
         }
     }
+    
 
     for(int i=0 ; i < NUM_OF_DIGITS ; i++){
         printf("%d ",counter[i]);
